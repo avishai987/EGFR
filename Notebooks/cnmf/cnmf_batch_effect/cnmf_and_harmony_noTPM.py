c9805f968d3f36835fd9b59267581eb70573c496
avishai987 <avishai987@gmail.com>
2024-04-16 17:01:28 +0300

    16/6/24

---
+++ Notebooks/cnmf/cnmf_batch_effect/cnmf_and_harmony_noTPM.py
@@ -0,0 +1,33 @@
+# to run, open onteractive job and:
+# source /sci/labs/yotamd/lab_share/avishai.wizel/python_envs/Virtual_env/cnmf_dev/bin/activate.csh
+# cd "/sci/labs/yotamd/lab_share/avishai.wizel/R_projects/EGFR/Notebooks/cnmf/cnmf_batch_effect/"
+# python cnmf_and_harmony_noTPM.py
+
+import os
+import pandas as pd
+import numpy as np
+from scipy.io import mmread
+import scipy.sparse as sp
+import matplotlib.pyplot as plt
+import scanpy as sc
+from cnmf import cNMF, Preprocess
+import seaborn as sns
+
+adata = sc.read("/sci/labs/yotamd/lab_share/avishai.wizel/R_projects/EGFR/Data/cnmf_V2/xeno_counts_filtered.h5ad")
+p = Preprocess(random_seed=14)
+print ("harmony...")
+(adata_c, adata_tp10k, hvgs) = p.preprocess_for_cnmf(adata, harmony_vars='orig.ident',theta = 2, n_top_rna_genes = 2000,max_scaled_thresh = 50, quantile_thresh = None,librarysize_targetsum = None, makeplots=True,save_output_base='/sci/labs/yotamd/lab_share/avishai.wizel/R_projects/EGFR/Data/cnmf_V2/noTPM/xeno_batchCorrect')
+
+print ("cnmf prepering...")                                          
+cnmf_obj_corrected = cNMF(output_dir='/sci/labs/yotamd/lab_share/avishai.wizel/R_projects/EGFR/Data/cnmf_V2/noTPM/', name='BatchCorrected_cnmf')
+cnmf_obj_corrected.prepare(counts_fn='/sci/labs/yotamd/lab_share/avishai.wizel/R_projects/EGFR/Data/cnmf_V2/noTPM/xeno_batchCorrect.Corrected.HVG.Varnorm.h5ad', genes_file='/sci/labs/yotamd/lab_share/avishai.wizel/R_projects/EGFR/Data/cnmf_V2/noTPM/xeno_batchCorrect.Corrected.HVGs.txt',components=np.arange(3,11), n_iter=20, seed=14, num_highvar_genes=2000)
+
+print ("factorizing...")
+cnmf_obj_corrected.factorize(worker_i=0, total_workers=1)
+print ("combining...")
+cnmf_obj_corrected.combine()
+print ("done...")
+import pickle
+f = open('/sci/labs/yotamd/lab_share/avishai.wizel/R_projects/EGFR/Data/cnmf_V2/noTPM/models_2Kvargenes_corrected_noTPM_cnmf_obj.pckl', 'wb')
+pickle.dump(cnmf_obj_corrected, f)
+f.close()
