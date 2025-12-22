195ed3ccf28eeaf9fbddfe373df521e753192424
avishai987 <avishai987@gmail.com>
2025-12-22 16:29:21 +0200

    add envs

---
+++ envs/install_cran_packages.R
@@ -0,0 +1,5 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+install.packages("MakefileR")
+devtools::install_github("avishai987/print.rmd.tabs")
+devtools::install_github("avishai987/SourceFromGithub")
+devtools::install_github("montilab/hypeR@e407bf1", dependencies = F) # cannot install with conda due to https://github.com/montilab/hypeR/issues/58
